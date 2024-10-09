23a37fa73ffae60b19802707c3cd90c4abbffa02
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2024-10-08 22:24:05 -0300

    Construção e estilização do menu retrátil mobile

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const menuRetratilMobile = document.getElementById("menu-retratil-mobile");
+
+function abrirMenu(){
+    menuRetratilMobile.style.height = "70vh"
+}
+
+function fecharMenu(){
+    menuRetratilMobile.style.height = "0px"
+}
