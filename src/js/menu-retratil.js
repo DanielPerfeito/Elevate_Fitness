d0c957be54aafc88c5c28cb19d3cb748de71bb65
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2025-01-03 23:06:51 -0300

    Conclusão da implementação da lógica do formulário de contato com FormSubimit

--- src/js/menu-retratil.js
+++ src/js/menu-retratil.js
@@ -1,9 +1,9 @@
 export const btnAbrirMenu = document.getElementById("btn-menu-retratil-abrir");
 export const btnFecharMenu = document.getElementById("btn-menu-retratil-fechar");
 export const linksMenuRetratil = document.querySelectorAll(".links-menu-retratil");
-
 export const menuRetratilMobile = document.getElementById("menu-retratil-mobile");
 
+// Funções principais
 export function abrirMenu() {
     menuRetratilMobile.style.height = "60vh";
 }
