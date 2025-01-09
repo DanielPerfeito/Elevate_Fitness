c4498f0baae890a963d96ad8e995c2ad88768f32
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2025-01-09 00:18:56 -0300

    Implementação das animações de Texto e Scroll

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,29 @@ import * as calcIMC from './calc-imc.js';
 
 calcIMC.btnCalcular.addEventListener("click", calcIMC.calcularIMC);
 calcIMC.btnRecalcular.addEventListener("click", calcIMC.reiniciarFormulario);
+
+
+// Animação de texto digitando (Via CDN)
+new Typewriter('#h1-2', {
+    strings: ['Eleve-se, Supere-se, Transforme-se!'],
+    autoStart: true,
+    loop: true,
+    cursor: ""
+});
+
+new Typewriter('#h1-1', {
+    strings: ['Eleve-se,', 'Supere-se,', 'Transforme-se!'],
+    autoStart: true,
+    loop: true,
+    cursor: ""
+});
+
+// Animaçoes de Scrool (Via CDN)
+
+const scroll = ScrollReveal({ reset: true });
+
+scroll.reveal('.para-revelar-top', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'bottom'
+})
